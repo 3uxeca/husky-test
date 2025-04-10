65d0c462d2b19d99cc94f28da21544687494c84e
3uxeca <sajihye4@gmail.com>
2025-04-10 10:48:37 +0900

    pre-commit test

---
+++ src/app/components/TestPreCommit.tsx
@@ -0,0 +1,12 @@
+// src/components/TestPreCommit.tsx
+const Test = () => {
+  const doSomething = (value: string) => {
+    console.log(value);
+  };
+
+  doSomething('hello');
+
+  return <div>Test</div>;
+};
+
+export default Test;
